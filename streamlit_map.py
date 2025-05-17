eb8f2e3998d9f4f7c72e22e04a2dffe0176f1500
Francisco Abraão <75095166+Fagoneto@users.noreply.github.com>
2025-05-17 17:01:14 -0300

    atualizacao

--- streamlit_map.py
+++ streamlit_map.py
@@ -11,80 +11,75 @@ from plotly.subplots import make_subplots
 st.set_page_config(layout='wide', page_title="Painel dos Preços")
 st.title("Painel dos Preços")
 
-# Carregar os dados das lojas e dos produtos
-df_lojas = pd.read_excel('lista_lojas.xlsx')
-df_lojas.columns = df_lojas.columns.str.strip().str.lower()
+           
+st.write("Escolha uma planilha: ")
+select_planilha = st.selectbox("Lista de Planilhas", ['precos_carrefour_kani_20250516_tratado.xlsx', 'precos_carrefour_empanado_20250516_tratado.xlsx'])
+if not select_planilha:
+    st.error("Por favor, escolha pelo menos uma planilha.")
+else:
+    df = pd.read_excel(select_planilha)
+             
+#df = pd.read_excel('precos_carrefour_kani_20250516_tratado.xlsx')
+
+#st.write(list(df['produto'].unique()))
+st.write("Data de coleta: ", df['data'].max())
 
 
-df = pd.read_csv('precos_carrefour_kani.csv')
 
 
-#alterar a coluna preco de R$ 22,69 para 22.69 (float)
-df['preco'] = df['preco'].str.replace('R$', '').str.replace('.', '', regex=False).str.replace(',', '.', regex=False)
-df['preco'] = df['preco'].astype(float) 
+# # Mapeamento de estados para regiões
+# uf_to_region = {
+#     'AC': 'Norte', 'AP': 'Norte', 'AM': 'Norte', 'PA': 'Norte', 'RO': 'Norte', 'RR': 'Norte', 'TO': 'Norte',
+#     'AL': 'Nordeste', 'BA': 'Nordeste', 'CE': 'Nordeste', 'MA': 'Nordeste', 'PB': 'Nordeste', 'PE': 'Nordeste',
+#     'PI': 'Nordeste', 'RN': 'Nordeste', 'SE': 'Nordeste',
+#     'DF': 'Centro-Oeste', 'GO': 'Centro-Oeste', 'MT': 'Centro-Oeste', 'MS': 'Centro-Oeste',
+#     'ES': 'Sudeste', 'MG': 'Sudeste', 'RJ': 'Sudeste', 'SP': 'Sudeste',
+#     'PR': 'Sul', 'RS': 'Sul', 'SC': 'Sul'
+# }
 
-#renomear coluna cidade_estado para cidade/UF do dataframe df
-df.rename(columns={'cidade_estado': 'cidade/UF'}, inplace=True)  
-#substituir " - " por "/" na coluna cidade/UF do dataframe df
-df['cidade/UF'] = df['cidade/UF'].str.replace(' - ', '/')  
-#separa o bairro de cidade/UF
-df['bairro'] = df['cidade/UF'].str.split(',', expand=True)[0]
-df['cidade/UF'] = df.apply(lambda row: row['cidade/UF'].replace(row['bairro'], '').strip(', '), axis=1)
-# junta bairro a loja
-df['loja'] = df['loja']+df['bairro']
+# # Criar a nova coluna com base no mapeamento
+# df['regiao'] = df['uf'].map(uf_to_region)
 
-#criando a coluna UF
-df['UF'] = df['cidade/UF'].str[-2:]
+# df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y')
 
-# Mapeamento de estados para regiões
-uf_to_region = {
-    'AC': 'Norte', 'AP': 'Norte', 'AM': 'Norte', 'PA': 'Norte', 'RO': 'Norte', 'RR': 'Norte', 'TO': 'Norte',
-    'AL': 'Nordeste', 'BA': 'Nordeste', 'CE': 'Nordeste', 'MA': 'Nordeste', 'PB': 'Nordeste', 'PE': 'Nordeste',
-    'PI': 'Nordeste', 'RN': 'Nordeste', 'SE': 'Nordeste',
-    'DF': 'Centro-Oeste', 'GO': 'Centro-Oeste', 'MT': 'Centro-Oeste', 'MS': 'Centro-Oeste',
-    'ES': 'Sudeste', 'MG': 'Sudeste', 'RJ': 'Sudeste', 'SP': 'Sudeste',
-    'PR': 'Sul', 'RS': 'Sul', 'SC': 'Sul'
-}
+# df = df[['produto', 'preco', 'cidade', 'uf', 'regiao', 'loja', 'lat', 'long', 'desconto']]
 
-# Criar a nova coluna com base no mapeamento
-df['regiao'] = df['UF'].map(uf_to_region)
 
-df = df[['data','produto', 'preco', 'cidade/UF', 'UF', 'regiao', 'loja', 'bairro']]
-
-df_maps = df.merge(df_lojas[['lat', 'long','loja']], how='left', left_on='loja', right_on='loja')
 
 #### ATENÇÃO TEMLOJAS QUE FALTA GEOLOCALIZACAO
 #excluir dados NaN
-df_maps = df_maps.dropna()
+#df = df.dropna()
 
-#data_load_state = st.text('Loading data...')
-st.write("Data de coleta: ",df_maps['data'].max())
+
+# #data_load_state = st.text('Loading data...')
+# st.write("Data de coleta: ")
+# st.write (df['data'].max())
 # Create a text element and let the reader know the data is loading.
 
 
-def to_excel(dados):
-    saida  = BytesIO()
-    writer = pd.ExcelWriter(saida, engine='xlsxwriter')
-    dados.to_excel(writer, index=False, sheet_name='Sheet1')
-    worksheet = writer.sheets['Sheet1']
-    worksheet.set_column('A:A', None)  
-    writer.save()
-    dados_excel = saida.getvalue()
-    return dados_excel
+# def to_excel(dados):
+#     saida  = BytesIO()
+#     writer = pd.ExcelWriter(saida, engine='xlsxwriter')
+#     dados.to_excel(writer, index=False, sheet_name='Sheet1')
+#     worksheet = writer.sheets['Sheet1']
+#     worksheet.set_column('A:A', None)  
+#     writer.save()
+#     dados_excel = saida.getvalue()
+#     return dados_excel
 
-#st.write(precos_carrefour_prod.head(5))
+# #st.write(precos_carrefour_prod.head(5))
 
 
 # # Criando DataFrame por Produto
 # ### Encontrando os produtos que ocorrem com mais frequencia
-# occur = df_maps.groupby(['produto']).count()
+# occur = df.groupby(['produto']).count()
 # c = occur.sort_values(by=['data'], ascending=False).reset_index()
 # c = c[['produto', 'preco', 'estado']]
 # c = c.rename(columns={'preco': 'Frequência'})
 
 # # Criando DataFrame por Cidade
 # ### Encontrando os produtos que ocorrem com mais frequencia
-# occur2 = df_maps.groupby(['cidade/UF']).count().reset_index()
+# occur2 = df.groupby(['cidade/UF']).count().reset_index()
 # d = occur2.sort_values(by=['data'], ascending=False)
 # d = d[['cidade/UF', 'data', 'estado']]
 # d = d.rename(columns={'data': 'Frequência'})
@@ -94,12 +89,12 @@ def to_excel(dados):
 #     col1, col2 = st.columns(2)
 
 #     with col1:
-#         num_pro = str(len(df_maps.produto.unique()))
+#         num_pro = str(len(df.produto.unique()))
 #         st.write("Número de produtos disponíveis: "+ num_pro, c)
 
 
 #     with col2:
-#         num_cidades = str(len(df_maps['cidade/UF'].unique()))
+#         num_cidades = str(len(df['cidade/UF'].unique()))
 #         st.write("Número de cidades coletadas: "+ num_cidades, d)
     
 
@@ -108,11 +103,11 @@ col1, col2 = st.columns(2)
 
 with col1:
     produto_1 = st.selectbox(
-            "Escolha quantos produtos quiser", (list(df_maps['produto'].unique())))#, [df_maps['produto'][0]])#, [## colocar aqui os primeiros itens da lista]
+            "Escolha quantos produtos quiser", (list(df['produto'].unique())))#, [df['produto'][0]])#, [## colocar aqui os primeiros itens da lista]
     if not produto_1:
         st.error("Por favor, escolha pelo menos um produto.")
     else:
-        df_maps_1 = df_maps[df_maps['produto']==produto_1]
+        df_1 = df[df['produto']==produto_1]
 
         # Criar um seletor para escolher se deseja exibir o preço ao lado do círculo
         exibir_preco = st.checkbox("Exibir preço ao lado do círculo")
@@ -121,12 +116,12 @@ with col1:
         mapa = folium.Map(location=[-15.793889, -47.882778], zoom_start=4)
 
         # Definir a escala de cores para os preços
-        preco_minimo = df_maps_1['preco'].min()
-        preco_maximo = df_maps_1['preco'].max()
+        preco_minimo = df_1['preco'].min()
+        preco_maximo = df_1['preco'].max()
         colormap = linear.YlOrRd_09.scale(preco_minimo, preco_maximo)
 
         # Adicionar círculos para representar os preços das lojas
-        for _, row in df_maps_1.iterrows():
+        for _, row in df_1.iterrows():
             circle = folium.CircleMarker(location=[row['lat'], row['long']], radius=10, color='black', fill=True, 
                                         fill_opacity=0.7, fill_color=colormap(row['preco']))
             
@@ -154,11 +149,11 @@ with col1:
 
 with col2:
     produto_2 = st.selectbox(
-            "Escolha produto seguinte:", (list(df_maps['produto'].unique())))#, [df_maps['produto'][0]])#, [## colocar aqui os primeiros itens da lista]
+            "Escolha produto seguinte:", (list(df['produto'].unique())))#, [df['produto'][0]])#, [## colocar aqui os primeiros itens da lista]
     if not produto_2:
         st.error("Por favor, escolha pelo menos um produto.")
     else:
-        df_maps_2 = df_maps[df_maps['produto']==produto_2]
+        df_2 = df[df['produto']==produto_2]
 
          # Criar um seletor para escolher se deseja exibir o preço ao lado do círculo
         exibir_preco = st.checkbox("Exibir preço ao lado do círculo.")
@@ -167,12 +162,12 @@ with col2:
         mapa = folium.Map(location=[-15.793889, -47.882778], zoom_start=4)
 
         # Definir a escala de cores para os preços
-        preco_minimo = df_maps_2['preco'].min()
-        preco_maximo = df_maps_2['preco'].max()
+        preco_minimo = df_2['preco'].min()
+        preco_maximo = df_2['preco'].max()
         colormap = linear.YlOrRd_09.scale(preco_minimo, preco_maximo)
 
         # Adicionar círculos para representar os preços das lojas
-        for _, row in df_maps_2.iterrows():
+        for _, row in df_2.iterrows():
             circle = folium.CircleMarker(location=[row['lat'], row['long']], radius=10, color='black', fill=True, 
                                         fill_opacity=0.7, fill_color=colormap(row['preco']))
             
@@ -199,14 +194,14 @@ with col2:
 
 
 
-        df_maps_3 = df_maps[df_maps['produto'].isin([produto_1, produto_2])]
+        df_3 = df[df['produto'].isin([produto_1, produto_2])]
         # Mesclar os DataFrames com base no nome da loja
-        df = pd.merge(df_lojas[['lat', 'long', 'loja']], df_maps_3, on='loja')
+        #df = pd.merge(df_lojas[['lat', 'long', 'loja']], df_3, on='loja')
 
-fig = px.scatter(df, x="cidade/UF", y='preco', color='produto', title= "Registro Diário de Preços",  hover_data=df.columns.unique())
+fig = px.scatter(df_3, x="cidade", y='preco', color='produto', title= "Registro Diário de Preços",  hover_data=df.columns.unique())
 st.plotly_chart(fig, use_container_width = True,height=800)
 
-fig2 = px.box(df, x="produto", y='preco', color='produto', title="Variação de Preços",  hover_data=df.columns.unique())
+fig2 = px.box(df_3, x="produto", y='preco', color='produto', title="Variação de Preços",  hover_data=df.columns.unique())
 st.plotly_chart(fig2, use_container_width = True,height=400)
 
 #down9 = to_excel(df)
@@ -246,9 +241,9 @@ for i in range(0, len(regioes), 2):
     # Primeira coluna
     with col1:
         regiao = regioes[i]
-        df_reg = df[df['regiao'] == regiao]
+        df_reg = df_3[df_3['regiao'] == regiao]
         fig = px.scatter(df_reg,
-                         x='cidade/UF',
+                         x='cidade',
                          y='preco',
                          color='produto',
                          title=f'Região {regiao}',
@@ -268,9 +263,9 @@ for i in range(0, len(regioes), 2):
     if i + 1 < len(regioes):
         with col2:
             regiao = regioes[i + 1]
-            df_reg = df[df['regiao'] == regiao]
+            df_reg = df_3[df_3['regiao'] == regiao]
             fig = px.scatter(df_reg,
-                             x='cidade/UF',
+                             x='cidade',
                              y='preco',
                              color='produto',
                              title=f'Região {regiao}',
